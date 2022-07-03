6a88da64dbf093c0cbd93ab0ed5e4efbac11670d
Toni Vucic <tonivuc@gmail.com>
2022-07-03 10:28:14 +0300

    Installed prettier and ran it on mainNav

--- frontend/src/components/mainNav.jsx
+++ frontend/src/components/mainNav.jsx
@@ -1,33 +1,38 @@
 import React from "react";
-import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import { Link, NavLink } from "react-router-dom";
-
 
 export default function MainNav() {
   return (
     <Navbar bg="light" expand="lg">
-        <Container>
-            <Navbar.Brand as={Link} to="/">Lunch system</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto" justify variant="tabs" defaultActiveKey="/">
-                <Nav.Item>
-                  <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link as={NavLink} to="/receipt-upload">Test</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="link-2">Link</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="disabled" disabled>
-                    Disabled
-                  </Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Navbar.Collapse>
-        </Container>
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          Lunch system
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto" justify variant="tabs" defaultActiveKey="/">
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/">
+                Home
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/receipt-upload">
+                Test
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-2">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="disabled" disabled>
+                Disabled
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
