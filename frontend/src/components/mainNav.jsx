21d41fdf7533771a7d89ed73ab940b4c646c1305
Toni Vucic <tonivuc@gmail.com>
2022-07-03 10:17:49 +0300

    Added Tabs to navbar

--- frontend/src/components/mainNav.jsx
+++ frontend/src/components/mainNav.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
+import { Link, NavLink } from "react-router-dom";
 
 
 export default function MainNav() {
   return (
     <Navbar bg="light" expand="lg">
         <Container>
-            <Navbar.Brand href="/">Lunch system</Navbar.Brand>
+            <Navbar.Brand as={Link} to="/">Lunch system</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/receipt-upload">Receipt upload</Nav.Link>
-            </Nav>
+              <Nav className="me-auto" justify variant="tabs" defaultActiveKey="/">
+                <Nav.Item>
+                  <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={NavLink} to="/receipt-upload">Test</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="link-2">Link</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="disabled" disabled>
+                    Disabled
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
             </Navbar.Collapse>
         </Container>
     </Navbar>
