252a315d34f3c5487b8e3e259620df3a02d7ca79
Toni Vucic <tonivuc@gmail.com>
2022-07-04 13:54:29 +0300

    WIP

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,20 +1,63 @@
 import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { loginUser } from "../api/auth";
+import { useState } from "react";
 
 function Login() {
+  const navigate = useNavigate();
+
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleResponse = (response) => {
+    const { token, user } = response;
+    navigate("/tasks");
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const response = await loginUser(values.username, values.password);
+    handleResponse(response);
+  };
+
+  const handleUsernameInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      username: event.target.value,
+    }));
+  };
+
+  const handlePasswordInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      password: event.target.value,
+    }));
+  };
+
   return (
     <div style={{ maxWidth: "300px" }}>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" />
+          <Form.Control
+            type="text"
+            value={values.firstName}
+            onChange={handleUsernameInputChange}
+          />
         </Form.Group>
         <Form.Label htmlFor="inputPassword5">Password</Form.Label>
         <Form.Control
           type="password"
           id="inputPassword5"
           aria-describedby="passwordHelpBlock"
+          value={values.password}
+          onChange={handlePasswordInputChange}
         />
         <div className="d-flex justify-content-end">
           <Button className="mt-3" type="submit">
