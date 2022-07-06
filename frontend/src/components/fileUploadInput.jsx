12791c23cc09495a698208aefcd72bf11d371785
Toni Vucic <tonivuc@gmail.com>
2022-07-06 13:40:01 +0300

    Display uploaded files in browser and send to server

--- frontend/src/components/fileUploadInput.jsx
+++ frontend/src/components/fileUploadInput.jsx
@@ -1,19 +1,18 @@
 import { useState } from "react";
 import { Form } from "react-bootstrap";
 
-const FileUploadInput = (onSelectedFileChange) => {
-  console.log(onSelectedFileChange);
+const FileUploadInput = (props) => {
   // On file select (from the pop up)
   const onFileChange = (event) => {
     console.log("OnFileChange");
     // Update the state
-    onSelectedFileChange(event.target.files[0]);
+    props.onSelectedFileChange(props.inputNr, event.target.files[0]);
   };
 
   return (
     <Form.Group controlId="formFile" className="mb-3">
       <Form.Label>Upload file</Form.Label>
-      <Form.Control type="file" onChange={onFileChange} />
+      <Form.Control type="file" onChange={onFileChange} name="uploaded_file" />
     </Form.Group>
   );
 };
